9ce36611a07184014b17c5b71be7ab551d506891
liuting <942501339@qq.com>
2019-02-23 17:33:49 +0800

    提交

---
+++ src/pages/route_demo/route1/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {HashRouter, Route, Link} from 'react-router-dom'
+import './index.less'
+
+export default class Home extends React.Component{
+
+    render () {
+        return (
+            <HashRouter className="home-wrap">
+                
+            </HashRouter>
+        )
+    }
+}
