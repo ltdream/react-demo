821af7ec7b8298f702d5bcf64e50808fc9a02430
liuting <942501339@qq.com>
2019-03-04 16:20:41 +0800

    tijiao

--- src/router.js
+++ src/router.js
@@ -13,8 +13,10 @@ export default class IRouter extends React.Component{
                     <Route path="/login" component={Login}/>
                     <Route path="/admin" render={()=>
                         <Admin>
-                            <Route path="/admin/ui/buttons" component={Buttons}/>
-                            <Route component={nomatch}/>
+                            <Switch>
+                                <Route path="/admin/ui/buttons" component={Buttons}/>
+                                <Route component={nomatch}/>
+                            </Switch>
                         </Admin>
                     }/>
                 </App>
